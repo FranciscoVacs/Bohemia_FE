7ff336bcd4e267af1db342d4ef5b0c01d56365a4
Nicolás Salerno <amplificodora04@gmail.com>
2026-02-09 10:11:51 -0300

    purchase redirect component + mp integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GaleriaComponent } from './galeria/galeria.component';
 import { CompraComponent } from './compra/compra.component';
+import { PurchaseRedirectComponent} from './compra/purchase-redirect/purchase-redirect.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'galeria', component: GaleriaComponent },
     { path: 'compra', component: CompraComponent },
+    { path: 'success', component: PurchaseRedirectComponent },
     {
         path: 'admin',
         loadChildren: () => import('./admin/admin.routes').then(m => m.adminRoutes)
