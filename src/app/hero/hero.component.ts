47207ecb54e1c65e41bb1f286c691bb986d9573d
FranciscoVacs <franciscovacs@gmail.com>
2026-02-17 23:45:47 -0300

    agregue video a herosection

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit {
+  @ViewChild('heroVideo') heroVideo!: ElementRef<HTMLVideoElement>;
 
+  ngAfterViewInit(): void {
+    const video = this.heroVideo.nativeElement;
+    video.play().catch(() => {
+      // Autoplay blocked by browser, retry on user interaction
+      document.addEventListener('click', () => video.play(), { once: true });
+    });
+  }
 }
