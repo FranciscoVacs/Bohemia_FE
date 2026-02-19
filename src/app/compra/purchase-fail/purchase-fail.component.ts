340a44672717b8df532fee0d1f676bf1343d4496
Nicolás Salerno <amplificodora04@gmail.com>
2026-02-19 16:02:39 -0300

    minor changes to redirect and fail components

--- src/app/compra/purchase-fail/purchase-fail.component.ts
+++ src/app/compra/purchase-fail/purchase-fail.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'purchase-fail',
-    imports: [],
+    imports: [RouterLink],
     templateUrl: './purchase-fail.component.html',
     styleUrl: './purchase-fail.component.css'
 })
